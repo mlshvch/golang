fd4c8384ea4ce5d6846426cd3b6ad82d5e3260fd
mlshvch <malashevichdev@gmail.com>
2022-07-20 10:49:26 +0300

    Add CSV file reading

--- gophercises/exercise_1/main.go
+++ gophercises/exercise_1/main.go
@@ -2,10 +2,17 @@ package main
 
 import (
 	"flag"
+	"fmt"
+	"os"
 )
 
 func main() {
 	csvFilename := flag.String("csv", "problems.csv", "a CSV file for starting the quiz")
 	flag.Parse()
-	_ = csvFilename
+	file, err := os.Open(*csvFilename)
+	if err != nil {
+		fmt.Printf("Failed to open CSV file: %s\n", *csvFilename)
+		os.Exit(1)
+	}
+	_ = file
 }
